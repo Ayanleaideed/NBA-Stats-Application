7019803a6e3f9791e61b6e2e32c24879dec4d00b
Ayanleaideed <Ayanleaideed@outlook.com>
2024-05-10 00:04:51 -0500

    Updated vercel configuration and settings for depgit push origin main

--- sports_stats_app/sports_stats_app/urls.py
+++ sports_stats_app/sports_stats_app/urls.py
@@ -1,12 +1,10 @@
+# sports_stats_app/urls.py
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from django.views.generic import RedirectView
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stats.urls')),
-    # path('', RedirectView.as_view(url='stats/', permanent=False)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
