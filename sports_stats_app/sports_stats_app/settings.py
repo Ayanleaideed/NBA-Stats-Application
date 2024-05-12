1a07a0af540223f316817afe3598e68cf97fd8a0
Ayanleaideed <Ayanleaideed@outlook.com>
2024-05-12 01:24:25 -0500

    Fixed database bug

--- sports_stats_app/sports_stats_app/settings.py
+++ sports_stats_app/sports_stats_app/settings.py
@@ -96,6 +96,5 @@ USE_TZ = True
 # Static files settings
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
