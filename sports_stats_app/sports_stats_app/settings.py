a838201f82d27c21128a213ba1ac6179bdab0310
Ayanleaideed <Ayanleaideed@outlook.com>
2024-05-12 01:26:26 -0500

    Fixed database bug

--- sports_stats_app/sports_stats_app/settings.py
+++ sports_stats_app/sports_stats_app/settings.py
@@ -1,18 +1,17 @@
+# sports_stats_app/settings.py
+
 import os
 from pathlib import Path
 from datetime import datetime, timedelta
+from pathlib import Path
 import environ
 
-# Initialize environment variables
-env = environ.Env()
-environ.Env.read_env()  # reads the .env file
 
-# Base directory of the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Security and debugging settings
-SECRET_KEY = env('SECRET_KEY', default='django-insecure-default-key')
-DEBUG = env.bool('DEBUG', default=False)
+SECRET_KEY = 'django-insecure-z_zde1*fk)*_k$-cpoi)r-cb%23)y%=0j@n8h2tbou0be6^52#'
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'nba-stats-application.vercel.app',
@@ -22,7 +21,6 @@ ALLOWED_HOSTS = [
     'localhost'
 ]
 
-# Installed applications
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -33,7 +31,6 @@ INSTALLED_APPS = [
     'stats.apps.StatsConfig',
 ]
 
-# Middleware configuration
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -44,10 +41,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# URL configuration
 ROOT_URLCONF = 'sports_stats_app.urls'
 
-# Templates configuration
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -64,7 +59,6 @@ TEMPLATES = [
     },
 ]
 
-# WSGI application path
 WSGI_APPLICATION = 'sports_stats_app.wsgi.application'
 
 # Database configuration
@@ -72,14 +66,19 @@ DATABASES = {
     'default': env.db('DATABASE_URL'),
 }
 
-# Session configuration
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 now = datetime.now()
 midnight = datetime.combine(now.date() + timedelta(days=1), datetime.min.time())
 seconds_until_midnight = (midnight - now).seconds
 SESSION_COOKIE_AGE = seconds_until_midnight
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -87,14 +86,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Internationalization settings
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files settings
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Default primary key field type
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
